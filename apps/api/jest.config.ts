df13ad378879e0bb9cd2ba46b1dc873ddb7e93c3
slduarte90 <sidneyluizduarte@gmail.com>
2026-05-13 14:13:03 -0300

    chore(api): add jest config

---
+++ apps/api/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testRegex: '.*\\.spec\\.ts$',
+};
+
+export default config;
