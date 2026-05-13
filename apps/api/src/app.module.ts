de43e8bc3e70bfa6c64d34922c42ce3a08465a0a
slduarte90 <sidneyluizduarte@gmail.com>
2026-05-13 15:56:43 -0300

    feat(api): register companies module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { CompaniesModule } from './companies/companies.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule, CompaniesModule],
   controllers: [HealthController],
 })
 export class AppModule {}
