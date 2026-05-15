b624b1d42d3fb0c8dfaf54e2e67679ce4d99acf2
slduarte90 <sidneyluizduarte@gmail.com>
2026-05-15 11:22:08 -0300

    feat(api): register invitations module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,16 @@ import { AuthModule } from './auth/auth.module';
 import { CompaniesModule } from './companies/companies.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthController } from './health.controller';
+import { InvitationsModule } from './invitations/invitations.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule, CompaniesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+    CompaniesModule,
+    InvitationsModule,
+  ],
   controllers: [HealthController],
 })
 export class AppModule {}
