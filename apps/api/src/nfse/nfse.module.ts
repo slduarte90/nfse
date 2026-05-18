ef6a7ce92bf125ecd7482fd8feddec2cd270c817
slduarte90 <sidneyluizduarte@gmail.com>
2026-05-18 15:08:17 -0300

    feat(api): provide national nfse api service

--- apps/api/src/nfse/nfse.module.ts
+++ apps/api/src/nfse/nfse.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NfseController } from './nfse.controller';
+import { NfseNationalApiService } from './nfse-national-api.service';
 import { NfseService } from './nfse.service';
 
 @Module({
   controllers: [NfseController],
-  providers: [NfseService],
+  providers: [NfseService, NfseNationalApiService],
 })
 export class NfseModule {}
